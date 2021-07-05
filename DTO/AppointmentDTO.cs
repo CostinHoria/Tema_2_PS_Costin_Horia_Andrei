eaeeab4039e247746b890e4ce065ccad48b6e822
Costin Horia <horia98andrei@yahoo.com>
2021-07-05 13:56:19 +0300

    first

---
+++ DTO/AppointmentDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tema2_NoLogin.Models;
+
+namespace Tema2_NoLogin.DTO
+{
+    public class AppointmentDTO
+    {
+        public int Id { get; set; }
+        public String Name_client { get; set; }
+        public String Phone_number { get; set; }
+        public DateTime Appointment_date { get; set; }
+        public double Total { get; set; }
+        public List<Service> Services { get; set; }
+
+        public string AppointmentAsString { get; set; }
+    }
+}
